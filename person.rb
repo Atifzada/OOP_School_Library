1d213c81419d2d50d845c75c956d51159d3b9f8f
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-06-20 22:03:39 +0500

    Set id to random value

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_reader :id,:name, :age
   attr_accessor :name, :age
   def initialize(id, age, parent_permission = true, name = 'unknown')
-    @id = id
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
