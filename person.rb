cc624b410ab36764d3c7be3cc4e2354f6655b4d5
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-06-20 19:41:47 +0500

    Add person class

---
+++ person.rb
@@ -0,0 +1,20 @@
+class person
+    def initialize(id,  name = "unknown", age, parent_permission = true)
+        @id = id
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+      end
+
+    def of_age? 
+        @age >= 18
+    end  
+        
+    def can_use_services? 
+        @age >= 18 || @parent_permission
+    end  
+
+end
+
+n1 = Person.new(1, 19, 'Atif')
+puts n1.of_age?
